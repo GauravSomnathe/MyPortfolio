93146c43eebe8f03159f30bddc668f53002f8f2c
Gaurav <gauravsomnathe992@gmail.com>
2025-07-26 02:18:38 +0530

    server update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://backendportfolio-t0s2.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
